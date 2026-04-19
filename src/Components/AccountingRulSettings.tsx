e2d6ca5ea8af34ae9ff7ee7b8504822195870271
onlinelivesolution <ramzansr@gmail.com>
2026-04-19 14:13:18 +0600

    Change

--- src/Components/AccountingRulSettings.tsx
+++ src/Components/AccountingRulSettings.tsx
@@ -189,11 +189,16 @@ const AccountingRuleSettings = () => {
                                         }
                                         className="w-full h-[30px] px-2 pr-8 rounded border border-gray-400 text-sm appearance-none"
                                     >
+                                        <option value="totalAmount">Total Amount</option>
+                                        <option value="exclusiveAmount">Total Paid</option>
+                                        <option value="vatAmount">Total Applied</option>
+                                        <option value="discountAmount">Total Discount</option>
+                                        <option value="totalAmount">Total Unallocated</option>
                                         <option value="">Select Field</option>
                                         <option value="exclusiveAmount">Exclusive Amount</option>
                                         <option value="vatAmount">VAT Amount</option>
                                         <option value="discountAmount">Discount</option>
-                                        <option value="totalAmount">Total Amount</option>
+
 
                                     </select>
                                     <ChevronDown className="absolute right-2 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-500 pointer-events-none"
